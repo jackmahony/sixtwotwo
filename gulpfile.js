f863ccbc7ec96464bd930691ebddcf4790466758
jack <jackmahonyy@gmail.com>
2017-02-09 21:18:58 +1000

    Added personal website link to footer - Added Wow.js to homepage elements - fixed column Aligment on hompage

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('watch', function() {
 	gulp.watch(paths.compiled).on('change', browserSync.reload);
 
   browserSync.init({
-      proxy: "http://localhost:8888/sixtwotwo/"
+      proxy: "http://localhost/sixtwotwo/"
   });
 });
 
